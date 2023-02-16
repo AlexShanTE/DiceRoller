724ee2fea2fbed7593f922785420d3015680f55b
Alex <alexsid.ShanTE@gmail.com>
2023-02-16 21:41:30 +0300

    added dice roll animation

--- app/src/main/java/com/alex/sid/shante/diceroller/presentation/ui/gamescreen/GameViewModel.kt
+++ app/src/main/java/com/alex/sid/shante/diceroller/presentation/ui/gamescreen/GameViewModel.kt
@@ -1,11 +1,18 @@
 package com.alex.sid.shante.diceroller.presentation.ui.gamescreen
 
 import androidx.lifecycle.ViewModel
-import com.alex.sid.shante.diceroller.domain.models.Dice
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class GameViewModel : ViewModel() {
 
@@ -13,28 +20,38 @@ class GameViewModel : ViewModel() {
     val uiState: StateFlow<GameState> = _uiState.asStateFlow()
 
     fun rollThemAll() {
-        val newDiceList: List<Dice> = uiState.value.diceList.map { dice ->
-            dice.copy(
-                currentValue = (0 until dice.imageList.size).random(),
-                imageList = dice.imageList
-            )
-        }
-
-        _uiState.update { currentState ->
-            currentState.copy(diceList = newDiceList)
+        uiState.value.diceList.forEachIndexed { index, _ ->
+            rollDice(index)
         }
     }
 
     fun rollDice(index: Int) {
-        _uiState.update { currentState ->
-            val diceNewValue = (0 until currentState.diceList[index].imageList.size).random()
-            val newList = currentState.diceList.mapIndexed { i, dice ->
-                if (i == index) dice.copy(
-                    currentValue = diceNewValue,
-                    imageList = dice.imageList
-                ) else dice
+        tickerFlow(period = 50.milliseconds, duration = 1.seconds)
+            .onEach { period ->
+                _uiState.update { currentState ->
+                    val newDiceList = currentState.diceList.mapIndexed { i, dice ->
+                        if (i == index) dice.copy(
+                            currentValue = (0 until dice.imageList.size).random(),
+                            imageList = dice.imageList
+                        ) else dice
+                    }
+                    currentState.copy(diceList = newDiceList)
+                }
             }
-            currentState.copy(diceList = newList)
+            .launchIn(viewModelScope) // or lifecycleScope or other
+    }
+
+    private fun tickerFlow(
+        period: Duration,
+        duration: Duration,
+        initialDelay: Duration = Duration.ZERO
+    ) = flow {
+        var currentTime: Duration = 0.seconds
+        delay(initialDelay)
+        while (duration > currentTime) {
+            emit(duration)
+            delay(period)
+            currentTime += period
         }
     }
 }
